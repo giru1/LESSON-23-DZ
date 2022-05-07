201220513f52efdf88318403b29619eed70a25b6
Giru <yagofarov.vadim@mail.ru>
2022-05-07 23:50:34 +0500

    bags bags bags

--- app.py
+++ app.py
@@ -8,17 +8,20 @@ app = Flask(__name__)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
-payload = {
-    'file_name': 'apache_logs.txt',
-    'cmd1': 'filter',
-    'value1': 'GET',
-    'cmd2': 'map',
-    'value2': '0'
-}
+
+def other_command(iter_obj, query):
+    query_items = query.split('|')
+    result = iter(map(lambda v: v.strip(), iter_obj))
+    for item in query_items:
+        cmd_value = item.split(':')
+        command = cmd_value[0]
+        value = cmd_value[1]
+        result = build_query(result, command, value)
+    return result
 
 
-def build_query(it, cmd, value):
-    res = map(lambda v: v.strip(), it)
+def build_query(file_data, cmd, value):
+    res = map(lambda v: v.strip(), file_data)
     if cmd == "filter":
         res = filter(lambda v, txt=value: txt in v, res)
     if cmd == "map":
@@ -35,24 +38,35 @@ def build_query(it, cmd, value):
     return res
 
 
-@app.route("/perform_query")
+@app.route("/perform_query", methods=["POST", "GET"])
 def perform_query():
+
+    query = request.args.get("query")
     try:
-        cmd1 = request.args["cmd1"]
-        cmd2 = request.args["cmd2"]
-        value1 = request.args["value1"]
-        value2 = request.args["value2"]
-        file_name = request.args["file_name"]
+        cmd1 = request.args.get("cmd1")
+        cmd2 = request.args.get("cmd2")
+        value1 = request.args.get("value1")
+        value2 = request.args.get("value2")
+        file_name = request.args.get("file_name")
+
     except KeyError:
-        print(111)
-        raise BadRequest
+        raise BadRequest(description=f"111not found key")
     file_path = os.path.join(DATA_DIR, file_name)
+    print(111)
     if not os.path.exists(file_path):
-        return BadRequest(cescription=f"{file_name} was not found")
-    with open(file_path)as fd:
-        res = build_query(fd, cmd1, value1)
-        res = build_query(res, cmd2, value2)
-        content = '\n'.join(res)
-        print(content)
+        return BadRequest(description=f"222{file_name} was not found")
+
+    with open(file_path)as file:
+        if query is not None:
+            result = other_command(file, query)
+            content = '\n'.join(result)
+        else:
+            result = build_query(file, cmd1, value1)
+            result = build_query(file, cmd2, value2)
+            content = '\n'.join(result)
 
     return app.response_class(content, content_type="text/plain")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
